4766ceef147c19520d9aa8b576ae732fad239ae3
LucioHdz <luciohdz0@gmail.com>
2021-05-13 17:53:16 -0500

    Colores a la grafica y tamaños no completos

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from model.algorithms import DDA
 from bokeh.plotting import figure, output_file, show
+from bokeh.palettes import Spectral6
+from bokeh.transform import linear_cmap
 
 if __name__=='__main__':
     x = int(input('Ingrese x1:'))
@@ -11,3 +13,10 @@ if __name__=='__main__':
     obj = DDA(x,y,x2,y2)
 
     (x_vars,y_vars)=obj.print_chart()
+
+    # graficando el resultado del algoritmo
+    output_file('grafica.html')
+    fig= figure()
+    mapper = linear_cmap(field_name='y', palette=Spectral6 ,low=min(y_vars) ,high=max(y_vars))
+    fig.square(x_vars,y_vars,color=mapper)
+    show(fig)
